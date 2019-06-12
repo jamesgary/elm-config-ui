1be993f134e69d04044a9da1d6ea99747d9e8b84
James Gary <gumotaku@gmail.com>
2019-06-11 17:44:18 -0700

    Better output

--- bin/gen-config.sh
+++ bin/gen-config.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-elm make src/ConfigGenerator.elm --output tmp/tmp.js && node tmp/tmp.js > src/Config.elm
+# Silently compiles and writes to src/Config.elm
+# Only outputs error if there's a compilation error
+
+elm make src/ConfigGenerator.elm --output tmp/gen-config.js > /dev/null
+node tmp/gen-config.js > src/Config.elm 2>/dev/null
